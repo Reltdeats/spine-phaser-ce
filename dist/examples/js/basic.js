820cabaa960139c53f9f1ae7dff5b9bf89ff49ad
Ruben Garcia Vila <ruben.garcia@spacego.games>
2023-08-30 11:04:55 +0200

    updated build versions

--- dist/examples/js/basic.js
+++ dist/examples/js/basic.js
@@ -1,7 +1,7 @@
 /**
  * Copyright for the Phaser CE Spine Plugin software and its source files:
  * @author       Ruben García Vilà
- * @version      0.9.3
+ * @version      0.9.5
  * @copyright    2023 Ruben García Vilà.
  * @license      {@link https://github.com/Reltdeats/spine-phaser-ce/blob/master/LICENSE|MIT License}
  *
@@ -9,6 +9,6 @@
  * @author       Esoteric Software LLC
  * @copyright    Copyright (c) 2013-2023, Esoteric Software LLC
  * @licence      {@link https://github.com/EsotericSoftware/spine-runtimes/blob/master/LICENSE}
- * 
- */(()=>{const e=new Phaser.Game(800,600,Phaser.AUTO,"spine-phaser-ce-example",{preload:function(){e.plugins.add(SpinePlugin),e.load.crossOrigin="anonymous";e.load.spine("spineBoy",{image:"https://raw.githubusercontent.com/EsotericSoftware/spine-runtimes/4.1/spine-ts/spine-phaser/example/assets/spineboy-pma.png",atlas:"https://raw.githubusercontent.com/EsotericSoftware/spine-runtimes/4.1/spine-ts/spine-phaser/example/assets/spineboy-pma.atlas",binary:"https://raw.githubusercontent.com/EsotericSoftware/spine-runtimes/4.1/spine-ts/spine-phaser/example/assets/spineboy-pro.skel"})},create:function(){const t=e.add.spine(e.world.centerX,595,"spineBoy");t.scale.setTo(.7);const{animations:n}=t.animationStateData.skeletonData;let s;t.animationState.setAnimation(0,n[6].name,!0),e.time.events.loop(2500,(()=>{let e=Math.floor(Math.random()*(n.length-1));for(;e===s&&n.length>1;)e=Math.floor(Math.random()*(n.length-1));s=e,t.animationState.setAnimation(0,n[e].name,!0)}))}})})();
-//# sourceMappingURL=basic.js.map
+ *
+ */(() => { const n = new Phaser.Game(800, 600, Phaser.AUTO, 'spine-phaser-ce-example', { preload() { n.plugins.add(SpinePlugin), n.load.crossOrigin = 'anonymous'; const e = 'https://raw.githubusercontent.com/EsotericSoftware/spine-runtimes/4.1/spine-ts/spine-phaser/example/assets/'; const a = { image: ''.concat(e, 'spineboy-pma.png'), atlas: ''.concat(e, 'spineboy-pma.atlas'), binary: ''.concat(e, 'spineboy-pro.skel') }; n.load.spine('spineBoy', a); }, create() { const e = n.add.spine(n.world.centerX, 595, 'spineBoy'); e.scale.setTo(0.7); const { animations: a } = e.animationStateData.skeletonData; let t; e.animationState.setAnimation(0, a[6].name, !0), n.time.events.loop(2500, (() => { let n = Math.floor(Math.random() * (a.length - 1)); for (;n === t && a.length > 1;)n = Math.floor(Math.random() * (a.length - 1)); t = n, e.animationState.setAnimation(0, a[n].name, !0); })); } }); })();
+// # sourceMappingURL=basic.js.map
